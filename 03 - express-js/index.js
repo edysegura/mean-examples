cfc80ec7edc3db4e34130edfd8e7d72c6d5b2b89
unknown <aluno@LTI2-030172.local.inatel.br>
2015-08-19 10:03:42 -0300

    Updated index.js

--- 03 - express-js/index.js
+++ 03 - express-js/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get("/", function(req, res) {
 	//res.send('Hey there!');
 	res.redirect('/index.html');
@@ -9,8 +11,6 @@ app.get("/", function(req, res) {
 app.get("/edysegura", function(req, res) {
 	res.send('Hey there Edy Segura!');
 });
-
-app.use(express.static('public'));
 
 var server = app.listen(3000, function() {
 	console.log("Server started...");
