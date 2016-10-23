de14360a7e1eaedf6ad26b6e99d94c66b80bfad6
Edy Segura <ednardo@inatel.br>
2016-10-23 11:55:11 -0200

    Refactor module example

--- 05 - modules/index.js
+++ 05 - modules/index.js
@@ -4,6 +4,9 @@ myModule1.writeSomething();
 myModule1.writeSomething();
 myModule1.writeSomething();
 
+var myModule1_b = require('./myModule1'); //get from cache
+myModule1_b.writeSomething();
+
 var myModule2 = require('./myModule2');
 
 var instanceOne = myModule2('Dependency 1');
