eaf675421d11507500d62971e0f14b1e17d07191
Edy Segura <ednardo@inatel.br>
2016-10-24 11:49:17 -0200

    Added identation

--- 02 - http/http-server.js
+++ 02 - http/http-server.js
@@ -6,5 +6,8 @@ var server = http.createServer(function (request, response) {
 	response.end('This is a little server!\n');
 });
 
-function onStart() { console.log('Server started at http://localhost:3000') }
+function onStart() { 
+	console.log('Server started at http://localhost:3000') 
+}
+
 server.listen(3000, onStart);
